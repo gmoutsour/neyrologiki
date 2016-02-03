b92a9362acf267e74638ee4573c03972e954b0c6
gmoutsour <gmoutsouroufis@gmail.com>
2016-02-03 13:15:53 +0200

    update the eksetaseis form/schema
    update the eksetaseis form/schema

--- patient.js
+++ patient.js
@@ -38,19 +38,271 @@
 	var eksetashSchema = new Schema(
 	{
 		_patient : { type: Schema.Types.ObjectId, ref: 'Patient' },
-		neyrologiki_right : {
+		egkefalikes_syzigies : {
 			global : Boolean ,
-			check_one : Boolean , 
-			optiki_oksitita : String , 
-			optika_pedia : String
+			neyrologiki_right : {
+				global : Boolean ,
+				field_2 : {
+					check : Boolean , 
+					optiki_oksitita : String , 
+					optika_pedia : String ,
+					shmeiwseis : String 
+					},
+				field_3 : {
+					check : Boolean ,
+					kores : {
+						myhsh : Boolean ,
+						mydriash : Boolean
+						} ,
+					phwtokinhtiko : Boolean ,
+					aneklysh_blefarwn : Boolean ,
+					diplwpia : Boolean ,
+					shmeiwseis : String 
+					},
+				field_4 : {
+					check : Boolean , 
+					diplwpia : Boolean , 
+					shmeiwseis : String 
+					}, 
+				field_6 : {
+					check : Boolean , 
+					diplwpia : Boolean , 
+					shmeiwseis : String 
+					},
+				field_5 : {
+					check : Boolean , 
+					V1 : Boolean , 
+					V2 : Boolean , 
+					V3 : Boolean 
+					},
+				field_7 : {
+					check : Boolean , 
+					anw : Boolean , 
+					katw : Boolean , 
+					diataraxh_geyshs : Boolean , 
+					diataraxh_saliou_dakrywn : Boolean , 
+					shmeiwseis : String 
+					},
+				field_8 : {
+					check : Boolean , 
+					nystagmos : {
+						check :Boolean , 
+						shmeiwseis : String
+						},
+					romberg : {
+						check :Boolean , 
+						shmeiwseis : String
+						},
+					unterberger : {
+						check :Boolean , 
+						shmeiwseis : String
+						},
+					rinne : {
+						check :Boolean , 
+						shmeiwseis : String
+						},
+					weber : {
+						check :Boolean , 
+						shmeiwseis : String
+						},
+					shmeiwseis : String 
+					},
+				field_9 : {
+					check : Boolean , 
+					diataraxi_anypsews_malthakis_yperwas : { 
+						check :Boolean , 
+						shmeiwseis :String
+						},
+					diataraxi_theshs_stafylhs : { 
+						check :Boolean , 
+						shmeiwseis :String
+						},
+					diataraxi_kataposhs : { 
+						check :Boolean , 
+						shmeiwseis :String
+						},
+					diataraxi_fwnhshs : { 
+						check :Boolean , 
+						shmeiwseis :String
+						},
+					diataraxi_aisthhtikotitas_farygga : { 
+						check :Boolean , 
+						shmeiwseis :String
+						},
+					antanaklastiko_farygga : {
+						k_f : Boolean,
+						oxi : Boolean
+						},
+					shmeiwseis : String 
+					},
+				field_10 : {
+					check : Boolean , 
+					shmeiwseis : String 
+					},
+				field_11 : {
+					check : Boolean , 
+					sternokl : {
+						k_f : Boolean,
+						oxi : Boolean,
+						atrofia : Boolean
+						},
+					trapezoeidhs : {
+						k_f : Boolean,
+						oxi : Boolean,
+						atrofia : Boolean
+						},
+					shmeiwseis : String 
+					},
+				field_12 : {
+					check : Boolean , 
+					thesh_glwssas : {
+						k_f : Boolean,
+						oxi : Boolean,
+						},
+					shmeiwseis : String 
+					}
+			},
+			neyrologiki_left: {
+				global : Boolean ,
+				field_2 : {
+					check : Boolean , 
+					optiki_oksitita : String , 
+					optika_pedia : String ,
+					shmeiwseis : String 
+					},
+				field_3 : {
+					check : Boolean ,
+					kores : {
+						myhsh : Boolean ,
+						mydriash : Boolean
+						} ,
+					phwtokinhtiko : Boolean ,
+					aneklysh_blefarwn : Boolean ,
+					diplwpia : Boolean ,
+					shmeiwseis : String 
+					},
+				field_4 : {
+					check : Boolean , 
+					diplwpia : Boolean , 
+					shmeiwseis : String 
+					}, 
+				field_6 : {
+					check : Boolean , 
+					diplwpia : Boolean , 
+					shmeiwseis : String 
+					},
+				field_5 : {
+					check : Boolean , 
+					V1 : Boolean , 
+					V2 : Boolean , 
+					V3 : Boolean 
+					},
+				field_7 : {
+					check : Boolean , 
+					anw : Boolean , 
+					katw : Boolean , 
+					diataraxh_geyshs : Boolean , 
+					diataraxh_saliou_dakrywn : Boolean , 
+					shmeiwseis : String 
+					},
+				field_8 : {
+					check : Boolean , 
+					nystagmos : {
+						check :Boolean , 
+						shmeiwseis : String
+						},
+					romberg : {
+						check :Boolean , 
+						shmeiwseis : String
+						},
+					unterberger : {
+						check :Boolean , 
+						shmeiwseis : String
+						},
+					rinne : {
+						check :Boolean , 
+						shmeiwseis : String
+						},
+					weber : {
+						check :Boolean , 
+						shmeiwseis : String
+						},
+					shmeiwseis : String 
+					},
+				field_9 : {
+					check : Boolean , 
+					diataraxi_anypsews_malthakis_yperwas : { 
+						check :Boolean , 
+						shmeiwseis :String
+						},
+					diataraxi_theshs_stafylhs : { 
+						check :Boolean , 
+						shmeiwseis :String
+						},
+					diataraxi_kataposhs : { 
+						check :Boolean , 
+						shmeiwseis :String
+						},
+					diataraxi_fwnhshs : { 
+						check :Boolean , 
+						shmeiwseis :String
+						},
+					diataraxi_aisthhtikotitas_farygga : { 
+						check :Boolean , 
+						shmeiwseis :String
+						},
+					antanaklastiko_farygga : {
+						k_f : Boolean,
+						oxi : Boolean
+						},
+					shmeiwseis : String 
+					},
+				field_10 : {
+					check : Boolean , 
+					shmeiwseis : String 
+					},
+				field_11 : {
+					check : Boolean , 
+					sternokl : {
+						k_f : Boolean,
+						oxi : Boolean,
+						atrofia : Boolean
+						},
+					trapezoeidhs : {
+						k_f : Boolean,
+						oxi : Boolean,
+						atrofia : Boolean
+						},
+					shmeiwseis : String 
+					},
+				field_12 : {
+					check : Boolean , 
+					thesh_glwssas : {
+						k_f : Boolean,
+						oxi : Boolean,
+						},
+					shmeiwseis : String 
+					}
+			}
 		},
-		neyrologiki_left: {
+		kinitikotita : {
 			global : Boolean ,
-			check_one : Boolean , 
-			optiki_oksitita : String , 
-			optika_pedia : String
+		},
+		myiki_atrofia : {
+			global : Boolean ,
+		},
+		paregkefalidikes_dokimasies : {
+			global : Boolean ,
+		},
+		antanaklastika : {
+			global : Boolean ,
+		},
+		aisthhtikothta : {
+			global : Boolean ,
+		},
+		loipa : {
+			global : Boolean ,
 		}
-	  
 	});
 
 	
